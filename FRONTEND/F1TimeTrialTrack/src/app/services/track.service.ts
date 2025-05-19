f033b4f4d01d5d61c8025e198cc58bd2c56fe0b7
keseru.marton@stud.uni-obuda.hu <keseru.marton@stud.uni-obuda.hu>
2025-05-19 15:21:33 +0200

    serviceek kezdése

--- FRONTEND/F1TimeTrialTrack/src/app/services/track.service.ts
+++ FRONTEND/F1TimeTrialTrack/src/app/services/track.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Track } from '../models/track';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrackService {
 
-  constructor() { }
+  apiBaseUrl:string = "https://localhost:7263/"
+  tracks: Track[] = [];
+  constructor(private http:HttpClient) {
+    this.loadTracks();
+   }
+    loadTracks(): void {
+    this.http.get<Track[]>(this.apiBaseUrl+'Tracks').subscribe(data => {
+      this.tracks = data;
+    });
+  }
+  
 }
