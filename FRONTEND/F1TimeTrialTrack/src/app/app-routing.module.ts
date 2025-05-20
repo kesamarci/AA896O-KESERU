ca1b7462ba23816e24d037c9ae773ef97ca9f6f7
keseru.marton@stud.uni-obuda.hu <keseru.marton@stud.uni-obuda.hu>
2025-05-20 14:22:31 +0200

    trackadd

--- FRONTEND/F1TimeTrialTrack/src/app/app-routing.module.ts
+++ FRONTEND/F1TimeTrialTrack/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TtDeleteComponent } from './tts/tt-delete/tt-delete.component';
 import { TtListIdComponent } from './tts/tt-list-id/tt-list-id.component';
 import { TtEditComponent } from './tts/tt-edit/tt-edit.component';
 import { TtRatingComponent } from './tts/tt-rating/tt-rating.component';
+import { TrackAddComponent } from './tracks/track-add/track-add.component';
 
 const routes: Routes = [ 
   { path: 'tts', component: TtListComponent },
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: 'tt-edit/:id', component: TtEditComponent }, 
   {path:'tt-delete',component:TtDeleteComponent},
   { path: 'tt-rating', component: TtRatingComponent },
+  { path: 'track-create', component: TrackAddComponent },
 ]
 
 @NgModule({
--- FRONTEND/F1TimeTrialTrack/src/app/tracks/track-add/track-add.component.ts
+++ FRONTEND/F1TimeTrialTrack/src/app/tracks/track-add/track-add.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TrackService } from '../../services/track.service';
+import { Router } from '@angular/router';
+import { Track } from '../../models/track';
 
 @Component({
   selector: 'app-track-add',
@@ -7,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrl: './track-add.component.sass'
 })
 export class TrackAddComponent {
+ track: Track = new Track();
 
+  constructor(private trackService: TrackService, private router: Router) {}
+
+  save(): void {
+    this.trackService.addTrack(this.track);
+    this.router.navigate(['/track-list']);
+  }
 }
+
