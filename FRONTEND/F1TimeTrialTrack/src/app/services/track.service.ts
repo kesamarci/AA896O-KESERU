1e1a7a536b232b4c7faf8e5e243b64a8f76d7c7b
keseru.marton@stud.uni-obuda.hu <keseru.marton@stud.uni-obuda.hu>
2025-05-20 13:55:28 +0200

    updatedsservice callbackre hogy legyen adatom mire lekérem

--- FRONTEND/F1TimeTrialTrack/src/app/services/track.service.ts
+++ FRONTEND/F1TimeTrialTrack/src/app/services/track.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Track } from '../models/track';
 import { HttpClient } from '@angular/common/http';
 import { Trackrating } from '../models/trackrating';
+import { Trackdetails } from '../models/trackdetails';
 
 @Injectable({
   providedIn: 'root'
@@ -10,44 +11,56 @@ export class TrackService {
 
   apiBaseUrl:string = "https://localhost:7263/api/"
   tracks: Track[] = [];
+  trackDetails: Trackdetails[] = [];
+  selectedTrack?: Track;
+  selectedTrackDetails?: Trackdetails;
   constructor(private http:HttpClient) {
     this.loadTracks();
    }
-    loadTracks(): void {
-    this.http.get<Track[]>(this.apiBaseUrl+'Tracks').subscribe(data => {
+     loadTracks(callback?: () => void): void {
+    this.http.get<Track[]>(this.apiBaseUrl + 'Tracks').subscribe(data => {
       this.tracks = data;
+      if (callback) callback();
     });
   }
-   addTrack(track: Track): void {
+    addTrack(track: Track): void {
     this.http.post<Track>(this.apiBaseUrl + 'Tracks', track).subscribe(() => {
-      this.loadTracks(); // újratöltés
+      this.loadTracks();
     });
   }
-  getAllTracks(): void {//ugyan az kb mint a load de jobban látom igy
-  // Ez a metódus külön is hívható ha csak listát akarunk lekérni
-  this.http.get<Track[]>(this.apiBaseUrl + 'Tracks').subscribe(data => {
-    this.tracks = data;
-  });
+    getAllTracks(callback: (data: Track[]) => void): void {
+    this.http.get<Track[]>(this.apiBaseUrl + 'Tracks').subscribe(data => {
+      const tracks = data.map(item => {
+        const t = new Track();
+        Object.assign(t, item);
+        return t;
+      });
+      callback(tracks);
+    });
   }
   deleteTrack(id: string): void {
     this.http.delete(this.apiBaseUrl + 'Tracks/' + id).subscribe(() => {
-      this.loadTracks(); // újratöltés
+      this.loadTracks();
+    });
+  }
+    updateTrack(track: Track, callback?: () => void): void {
+    this.http.put(this.apiBaseUrl + 'Tracks/' + track.id, track).subscribe(() => {
+      const index = this.tracks.findIndex(x => x.id === track.id);
+      if (index !== -1) {
+        this.tracks[index] = track;
+      }
+      if (callback) callback();
+    });
+  }
+   getTrackById(id: string, callback: (data: Track) => void): void {
+    this.http.get<Track>(this.apiBaseUrl + 'Tracks/' + id).subscribe(data => {
+      callback(data);
+    });
+  }
+  getTrackDetailsById(id: string, callback: (data: Trackdetails) => void): void {
+    this.http.get<Trackdetails>(this.apiBaseUrl + 'Tracks/' + id).subscribe(data => {
+      callback(data);
     });
   }
 
-  updateTrack(id: string, track: Track): void {
-    this.http.put(this.apiBaseUrl + 'Tracks/' + id, track).subscribe(() => {
-      this.loadTracks(); // újratöltés
-    });
-  }
-
-  addRating(rating: Trackrating): void {
-    this.http.post(this.apiBaseUrl + 'TracksRating', rating).subscribe();
-  }
-
-  loadTrackById(id: string, callback: (track: Track) => void): void {
-    this.http.get<Track>(this.apiBaseUrl + 'Tracks/' + id).subscribe(track => {
-      callback(track);
-    });
-  }
 }
